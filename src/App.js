765c2483363ed7aea5055c247c2e26fcd24e315b
Dimitri Devoille <dimitri.wdev@gmail.com>
2021-10-15 20:06:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -113,7 +113,6 @@ const App = () => {
   const [notFound, setNotFound] = useState('');
   const location = useGeoLocation();
 
-  const apiKey = '1180e92d59b094d86eacd6e86686db5f';
   const defaultIcon = `http://openweathermap.org/img/wn/02d@2x.png`;
   const errorIcon = `http://openweathermap.org/img/wn/11d@2x.png`;
 
@@ -137,6 +136,7 @@ const App = () => {
   }
 
   useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
     if (submittedValue !== '') {
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${submittedValue}&units=metric&APPID=${apiKey}`)
         // .then(res => res.json())
@@ -150,6 +150,7 @@ const App = () => {
   }, [submittedValue])
 
   useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
     if (!location.error) {
       if (submittedValue === '' && location.loaded === true) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.coordinates.lat}&lon=${location.coordinates.lng}&units=metric&appid=${apiKey}`)
